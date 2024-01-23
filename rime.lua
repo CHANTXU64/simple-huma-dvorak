657869611d0bd4991800bababbaa383e6cbe8c96
CHANTXU64 <chantxu@outlook.com>
2024-01-23 22:57:07 +0800

    delete exe, pinyin

--- rime.lua
+++ rime.lua
@@ -1,5 +1,4 @@
 core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
 number_translator = require("number")
 calculator_translator = require("calculator_translator")  --简易计算器
-exe_processor = require("exe")  -- 网页启动器
 shijian2_translator = require("shijian2") -- 高级时间
